76d81f5b7cfb92687027eddb33a6b515312952d6
thebestname <thebestname@mail.com>
2013-03-05 15:06:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeApp::Application.config.secret_token = 'ac3b584000d3780b296fada68c9eb34b1a69219f25a1053081f68597f4b25e352ebdecfd5d5ffca91dbb863397d765943c94f9f415cff8a5580d3cc53eeeeef1'
