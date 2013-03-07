93eb15e3aa444c610b4de85481b2406c43acfb49
thebestname <thebestname@mail.com>
2013-03-07 13:03:10 -0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "BOOM! It's Awesome!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
 end
