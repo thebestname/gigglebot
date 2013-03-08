d32f8637082a5740f01a79a263b10b01853c714a
thebestname <thebestname@mail.com>
2013-03-08 13:20:28 -0800

    Implement most of microposting

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
 #  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
 #
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
